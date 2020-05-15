1d89f30485735fcf45a017eb32897bf27edd3081
annammattis <54290779+annammattis@users.noreply.github.com>
2020-05-14 21:45:31 -0400

    updated index.js

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.set("view engine", "pug");
  * Routes Definitions
  */
 app.get("/", (req, res) => {
-    res.status(200).send('index.html');
+    // res.status(200).send('index.html');
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
